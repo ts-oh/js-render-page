4f3a3384bfa9496e9fc839ebea4c06676725dbfc
ts-oh <oh.timmms@gmail.com>
2022-02-18 10:22:48 +0700

    rebuilt and removed html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,14 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: {
     index: './src/index.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Cool Restaurant',
-    }),
-  ],
 
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
@@ -25,10 +18,6 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ],
